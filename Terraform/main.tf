26445900efd01ddf7c5c10ada7d0bcd115985439
venkataswamy0907-cyber <venkataswamy0907@gmail.com>
2025-12-06 10:31:17 +0530

    Replace user_data with deploy.sh script

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -263,9 +263,7 @@ resource "aws_launch_template" "app_lt" {
 
   vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
- user_data = base64encode(templatefile("${path.module}/user_data.tpl", {
-  var1 = "foo"
-}))
+ user_data = filebase64("${path.module}/../scripts/deploy.sh")
 
 }
 
